2abf3f6eb9e4fe601a900cce2022359dea337d62
Paul Drews <drews.paul@gmail.com>
2013-11-12 11:54:36 +0100

    Untested commit of angular rate added to pan and log startup script

--- cb_tracker/scripts/follow_controller.py
+++ cb_tracker/scripts/follow_controller.py
@@ -6,6 +6,7 @@ import rospy
 import math
 import tf
 from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 from axis_camera.msg import Axis
@@ -35,12 +36,15 @@ class cbTracker:
         self.sizekp = rospy.get_param('~size_kp', 1.0)
         self.baseframe = rospy.get_param('~baseframe', "/world")
         self.cb_timeout = rospy.get_param('~timeout', 2.0)
+        self.imuScale = rospy.get_param('~imu_scale', 0.0)
         self.pub = rospy.Publisher("~twistOut",Twist)
         self.pubsim = rospy.Publisher("~rateOut",Float64)
         rospy.Subscriber("~cbSize",Float32,self.store_cbsize)
+        rospy.Subscriber("~IMU",Imu,self.store_imu)
         self.tfl = tf.TransformListener()
         self.cbsize = 0.2
         self.dyn_reconf_server = DynamicReconfigureServer(CameraControlConfig, self.reconfigure)
+        self.imuz = 0.0
 
     def reconfigure(self, config, level):
         rospy.loginfo("Got reconfigure request")
@@ -52,6 +56,9 @@ class cbTracker:
     def store_cbsize(self, data):
         #cbSize is actually the distance from the edge of the board to the edge of the camera frame
         self.cbsize = data.data
+
+        def store_imu(self, data):
+        self.imuz = data.angular_velocity.z
 
     def run(self):
         rospy.loginfo("Waiting for checkerboard...")
@@ -82,7 +89,7 @@ class cbTracker:
                 out.angular.z = self.kp_x * math.atan2(pt.point.x, pt.point.z)
                 #rospy.loginfo("Rate: %f", out.angular.z)
             else:
-                out.angular.z = self.kp_x * math.atan2(pt.point.y, pt.point.x)
+                out.angular.z = (self.kp_x * math.atan2(pt.point.y, pt.point.x)) + (imu_scale * self.imuz)
                 #rospy.loginfo("Rate: %f", out.angular.z)
 
             if (lastTime < (rospy.Time.now() - rospy.Duration(self.cb_timeout))):
@@ -90,6 +97,8 @@ class cbTracker:
                 rospy.loginfo("Lost the checkerboard, now %f lastTime %f", rospy.Time.now().to_sec(), lastTime.to_sec())
                 out.linear.x = -1.0
             rospy.loginfo("zoom = %f", out.linear.x)
+            # For now, don't zoom anywhere
+            out.linear.x = 0.0
             self.pub.publish(out)
             self.pubsim.publish(out.angular.z)
             rate.sleep()
