e0d8c2f63e6052cba0ff1ddce76a70c732a9bc2a
Paul Drews <drews.paul@gmail.com>
2014-02-12 11:00:15 +0100

    Added constant task scripts and updated log launch file

---
+++ rate_control/missions/forward.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# ROS specific imports
+import roslib; roslib.load_manifest('sim_tasks')
+import rospy
+from math import *
+from task_manager_lib.TaskClient import *
+from optparse import OptionParser
+
+rospy.init_node('task_client')
+server_node = rospy.get_param("~server","/task_server")
+default_period = rospy.get_param("~period",0.05)
+tc = TaskClient(server_node,default_period)
+
+parser = OptionParser()
+parser.add_option("-s",dest="speed", default=0.1
+                  help="Constant forward speed")
+
+(options, args) = parser.parse_args()
+
+tc.WaitForAuto()
+try:
+    tc.Constant(linear=options.speed,angular=0.0,duration=10.0)
+
+except TaskException, e:
+    rospy.logerr("Exception caught: " + str(e))
+
+if not rospy.core.is_shutdown():
+    tc.SetManual()
+
+
+rospy.loginfo("Mission completed")
+
+
